d98bb894d0c8fdec2711e8469c4ce6ec017ddd7b
Jaeho <sjaeho348@gmail.com>
2026-04-15 01:02:35 +0900

    fix : 이미지를 외부 경로에서 번들에 포함하도록 변경

--- src/screens/GenreSelectScreen.tsx
+++ src/screens/GenreSelectScreen.tsx
@@ -8,43 +8,37 @@ const GENRES: GenreCard[] = [
   {
     id: '아포칼립스',
     realityNumber: 'Reality 01',
-    imageUrl:
-      'https://lh3.googleusercontent.com/aida/ADBb0ujrTUgDnNZ1zWuF70Vg1dCh2UCW5ezxNiK3pMPV_2f50_hechYfFBWgHeuQndb_KoD93CnsGY_B8OUwYoxfUQkEpaOb_nIbpdwTqPUISYy8hd7qiD7oSR6m6taqx0up60wcZ2EP4ZrfQOOJDAJkwkAXHukGo8OhmDJDVRce1wiJ6kiau-43B4LmYBf8lrVyT7WrbIGGRyX6W4rR1qFGX1HqxDu3gNgGIx4d92ulNYaRuH6BSgwhISShwxh7VH6akhhuH-SygjZcSA',
+    imageUrl: '/genre/apocallipse.jpg',
     description: '황폐해진 세상 속에서의 생존 투쟁',
   },
   {
     id: '판타지',
     realityNumber: 'Reality 02',
-    imageUrl:
-      'https://lh3.googleusercontent.com/aida/ADBb0ujvQ7zhZj6skQLt01B5BYyVtfikONXmaCTX2xepW2p92OVjgq_WI2iOCwrnmt6U5AWN0Cq_kic22cR4xg8iAuYcQnmfAIkUgJbzAx15dcHPoCHqXzyLmMWFifONKttiqpmYKn6l9S8h_G2DK7fBLgF7MQkbsNQ-v9YXTqgUeN6bhay3val6OWUrhT608nD0Ii3h42o9vBtrkkYsBDfMkwarGT-cfHHlLDZtbr8tmmxChVW6N1IjYowXLjJMgLzaO1rwrXnXGd_cCA',
+    imageUrl: '/genre/판타지.jpg',
     description: '마법과 고대 신화가 살아 숨쉬는 대지',
   },
   {
     id: 'SF 탐사',
     realityNumber: 'Reality 03',
-    imageUrl:
-      'https://lh3.googleusercontent.com/aida/ADBb0uira4Sd35bQk7fhr6kBnw3VKYCo3QCaUt5fCi9Pqx0M0LqRD4KsNrkF6qUzoxxLNKyctl1T3QQLcAmTif1gM5_8czKt25-Y82qaU4zUiYpVERWVhA9W07uUUTWKeId4yn7VdcOzmA7fLPdM9KG8zEwGsHHbl8QlrYIYeFwsGDsuzlrjwyMsEemepSVwXTEzR31qHJ2AH8Tes5EYsC0TZtDcMTGHVLrTshvNb-5ksdzZQgoimohtzVvuzsNwy4AwFJgidAMcL8cy',
+    imageUrl: '/genre/sf.jpg',
     description: '광활한 우주를 향한 인류의 끝없는 도약',
   },
   {
     id: '공포',
     realityNumber: 'Reality 04',
-    imageUrl:
-      'https://lh3.googleusercontent.com/aida/ADBb0uiwPo9XrgZyO932fbQ7PU10Ns16BtMicWMutuJEOKSo66sQO7FCqN3nGVYqVQ9DwaUn_yDqAkwHisFhcU2leuHwafUrNU75ClnJHouemZ7s89uXJYhKIGD5HPfMY6qqFIDpT0INztTFQMsgvPEzn5cI3pc5uNeLoj-aIJQCjIJFL8Hr6KiWofCxsk2_4mLD-4mkB-7ONX1NMlyx5RfgArPBLyQMto4o2OH4IIl-ozmieNqg49Saqr2OIfl48a7lIe-QmkKMnP1wtQ',
+    imageUrl: '/genre/공포.jpg',
     description: '어둠 속에 도사리는 원초적인 공포',
   },
   {
     id: '코미디',
     realityNumber: 'Reality 05',
-    imageUrl:
-      'https://lh3.googleusercontent.com/aida/ADBb0uhDp1lg4sNtDjOFPHXs7BIcQNkZL1y5gdaErewivzmOI97rWPHxtLSjDAnOpJcq19hpGPLMZw6OHiNBG_TiGuG33t0QmRYAgrxPsG3afWPcoZD-2ZQopog2hZOTulP0raYJ_BtG9htONuh7unpMQCNxDtsnrc3mgRztyBFVzAtZfL6jxo-eFOI06A3ehWxGniG2yYGMoqKKTPQWynzjflRGNhxXhdeCtFtKV66HGmzXQTE7HfPMyvcRxJ_4UD0x9RzGKojsv2zn9Q',
+    imageUrl: '/genre/코미디.jpg',
     description: '예상치 못한 순간에 터져 나오는 유쾌함',
   },
   {
     id: '로맨스',
     realityNumber: 'Reality 06',
-    imageUrl:
-      'https://lh3.googleusercontent.com/aida/ADBb0ui7B5jH6aRc4nko3zAeDf8WL6laC6Wlwa_Gsr1-2d3bVXrnydm5KAReny2eYRmblVQHWpmQ9kLxHV8sD3DntklASbrxs9-6dlbOz3OU5eOyNDkGHl03PH73K4UBeH1D_N8PLUED2qNgffUMKQY2MDMwKENVry5mPc435NGmTtDQcMGrlp73KBDFw1gZFq0d5p388GBxfJpiQQcWSYl8IqMZxXhLtt05W4bLvTh9jpnCxB4elYI_pkGfriX-78YKjU6Oq2r31mQBlw',
+    imageUrl: '/genre/로맨스.avif',
     description: '운명적인 만남이 빚어내는 가슴 떨리는 이야기',
   },
 ]
